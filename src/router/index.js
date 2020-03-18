f16d00cbaeec0aaca9c6dad800a2a286ab8da83e
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-03-18 16:25:48 +0100

    Register routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Admin from "../views/Admin";
+import Account from "../views/Account";
+import Permission from "../views/Permission";
+import Cloud from "../views/Cloud";
+import History from "../views/History";
+import Settings from "../views/Settings";
+import Feedback from "../views/Feedback";
+import Help from "../views/Help";
+import Role from "../views/Role";
+import Storage from "../views/Storage";
 
 Vue.use(VueRouter)
 
@@ -9,6 +19,56 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/accounts',
+    name: 'Account',
+    component: Account
+  },
+  {
+    path: '/admins',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/roles',
+    name: 'Role',
+    component: Role
+  },
+  {
+    path: '/permissions',
+    name: 'Permission',
+    component: Permission
+  },
+  {
+    path: '/cloud',
+    name: 'Cloud',
+    component: Cloud
+  },
+  {
+    path: '/storage',
+    name: 'Storage',
+    component: Storage
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: History
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
+  },
+  {
+    path: '/feedback',
+    name: 'Feedback',
+    component: Feedback
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    component: Help
   },
   {
     path: '/about',
