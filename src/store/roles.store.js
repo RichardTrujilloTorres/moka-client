fce0f904b14daf635527bad806e7c14ce58e7121
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-04-26 07:27:13 +0200

    Add role store

---
+++ src/store/roles.store.js
@@ -0,0 +1,45 @@
+import RolesRepository from "../resources/repository/roles.repository";
+
+const Roles = {
+    namespaced: true,
+    state: {
+        roles: [],
+        currentRole: null,
+        repository: new RolesRepository(),
+    },
+    mutations: {
+        setRoles(state, payload) {
+            state.roles = payload;
+        },
+        setCurrentRole(state, payload) {
+            state.currentRole = payload;
+        },
+    },
+    actions: {
+        getRoles(context, params) {
+            return context.state.repository.index(params)
+                .then(res => context.commit('setRoles', res.data.data));
+        },
+        getRole(context, id) {
+            return context.state.repository.show(id)
+                .then(res => context.commit('setCurrentRole', res.data.data));
+        },
+        updateRole(context, {id, data}) {
+            return context.state.repository.update(id, data)
+                .then(res => context.commit('setCurrentRole', res.data.data));
+        },
+        deleteRole(context, id) {
+            return context.state.repository.delete(id);
+        },
+    },
+    getters: {
+        roles(state) {
+            return state.roles;
+        },
+        role(state) {
+            return state.currentRole;
+        },
+    },
+};
+
+export default Roles
