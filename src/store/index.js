25a7f64f0e6c4f44801cb86140846313364e9d6a
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-04-27 04:52:14 +0200

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Users from "./users.store";
+import Roles from "./roles.store";
 
 Vue.use(Vuex)
 
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   },
   modules: {
     user: Users,
+    role: Roles,
   }
 })
