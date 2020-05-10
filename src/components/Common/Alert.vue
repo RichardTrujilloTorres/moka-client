d9fe5c997bf14afe6f3c84c7715491dc893a35f8
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-05-10 16:38:10 +0200

    Add common alert component

---
+++ src/components/Common/Alert.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <v-alert
+            v-model="alert.show"
+            dismissible
+            elevation="2"
+            :type="alert.type"
+            @alert-show="onShow"
+        >
+            <div v-html="alert.message"></div>
+        </v-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            this.$eventBus.$on('alert-show', this.onShow)
+        },
+        data () {
+            return {
+                alert: {
+                    show: false,
+                    message: '',
+                    type: 'success',
+                },
+            }
+        },
+        methods: {
+            onShow({message, type}) {
+                this.alert.message = message;
+                this.alert.type = type;
+                this.alert.show = true;
+            }
+        }
+    }
+</script>
