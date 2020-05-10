12ed3ade109f2486883c9b37fc471b15c04e33b2
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-05-10 15:27:26 +0200

    Add role list

---
+++ src/components/Roles/RoleList.vue
@@ -0,0 +1,40 @@
+<template>
+    <list
+        title="Roles"
+        :delete-action="'role/deleteRole'"
+        :get-action="'role/getRoles'"
+        :items-getter="'role/roles'"
+        :fields="fields"
+        @item-create="onCreate"
+        @item-edit="onEdit"
+    >
+    </list>
+</template>
+
+<script>
+  import List from "../Common/List";
+  export default {
+    name: 'RoleList',
+      components: {List},
+    created() {
+    },
+    computed: {
+        fields() {
+            return [
+                {
+                    key: 'name',
+                    name: 'Name'
+                },
+            ]
+        }
+    },
+    methods: {
+      onEdit(id) {
+          this.$router.push({name: 'RoleDetail', params: {id}})
+      },
+      onCreate() {
+        console.log('create on role list')
+      },
+    },
+  }
+</script>
