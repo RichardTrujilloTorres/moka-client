30bd8fcaf23a4e368411f0e3b3f9405882a76ad0
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-06-06 09:51:04 +0200

    Add Echo setup

--- src/main.js
+++ src/main.js
@@ -15,3 +15,21 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
+
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    authEndpoint: 'http://localhost:8000/broadcasting/auth',
+  broadcaster: 'pusher',
+  key: process.env.VUE_APP_PUSHER_APP_KEY,
+  cluster: process.env.VUE_APP_PUSHER_APP_CLUSTER,
+  encrypted: true
+});
+
+window.Echo.private('notifications')
+    .listen('UserRegistered', (e) => {
+      console.log('Moka event');
+      console.log(e);
+    });
