592ce8066ce9a2f51c1a8a6da85a7627726cd017
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-22 08:47:41 +0200

    Add history components

---
+++ src/views/History/Change.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card
+            class="mx-auto"
+            max-width="800"
+    >
+        <v-card-text v-if="history">
+            <p>
+                <vue-json-pretty :data="history">
+                </vue-json-pretty>
+            </p>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    import VueJsonPretty from 'vue-json-pretty';
+
+    export default {
+        name: 'Change',
+        props: {
+            history: {
+                type: Object,
+                required: true,
+            }
+        },
+        components: {
+            VueJsonPretty,
+        },
+        created() {
+        },
+        data: () => ({
+        }),
+    }
+</script>
---
+++ src/views/History/ChangeList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-for="history in histories">
+            <change :history="history"></change>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapActions, mapGetters} from "vuex";
+    import Change from "./Change";
+
+    export default {
+        name: 'ChangeList',
+        components: {
+            Change
+
+        },
+        created() {
+            this.getHistories({
+                resource: 'permission',
+                params: {}
+            }); // TODO: prop
+        },
+        data: () => ({
+        }),
+        computed: {
+            ...mapGetters({
+                histories: 'histories/histories',
+            }),
+        },
+        methods: {
+            ...mapActions({
+                getHistories: 'histories/getHistories',
+            }),
+        }
+    }
+</script>
