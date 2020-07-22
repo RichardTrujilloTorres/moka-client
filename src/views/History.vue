0e2d2657a228bc04d7d0b3e63ed5b425b082c91d
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-22 08:47:56 +0200

    Add history components (cont.)

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="user">
       <h1>Resources History</h1>
+
+      <br/>
+
+      <change-list></change-list>
   </div>
 </template>
 
 <script>
 
+import ChangeList from "./History/ChangeList";
 export default {
-  name: 'Account',
+  name: 'History',
   components: {
+      ChangeList,
   }
 }
 </script>
