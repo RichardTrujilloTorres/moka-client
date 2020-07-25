f84b7e7fcb5da3409ff6efc4877a8c01a5d1d97d
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-25 07:49:16 +0200

    Update redirect interceptor

--- src/resources/interceptors/redirect.interceptor.js
+++ src/resources/interceptors/redirect.interceptor.js
@@ -1,9 +1,19 @@
+import {types} from "./types";
+import {app} from "../../main";
+
 const redirectInterceptor = {
     name: 'redirect',
+    type: types.response,
     config: (config) => {
         return config
     },
+    response: (res) => {
+        app.$eventBus.$emit('loaded');
+        return res
+    },
     error: (error) => {
+        console.log('redirect on specific status code');
+        app.$eventBus.$emit('loaded');
         return Promise.reject(error)
     },
 };
