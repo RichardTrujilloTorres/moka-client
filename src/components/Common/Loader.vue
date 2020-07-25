d74031c68ba6b4fc375cc322f385ba1bbefb5921
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-25 07:47:24 +0200

    Add loader component

---
+++ src/components/Common/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-center">
+        <v-overlay :value="loading">
+            <v-progress-circular indeterminate size="64"></v-progress-circular>
+        </v-overlay>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Loader',
+        created() {
+            this.$eventBus.$on('loading', () => this.loading = true)
+            this.$eventBus.$on('loaded', () => this.loading = false)
+        },
+        data () {
+            return {
+                loading: false,
+            }
+        },
+        methods: {
+        }
+    }
+</script>
