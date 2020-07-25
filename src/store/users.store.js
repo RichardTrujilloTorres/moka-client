9b5e7ea0d551b9d7ee3b1dab824fc01eeadf4b16
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-25 07:53:13 +0200

    Update users repository: add loading interceptor

--- src/store/users.store.js
+++ src/store/users.store.js
@@ -1,11 +1,14 @@
 import UsersRepository from "../resources/repository/users.repository";
+import loadingInterceptor from "../resources/interceptors/loading.interceptor";
 
 const Users = {
     namespaced: true,
     state: {
         users: [],
         currentUser: null,
-        repository: new UsersRepository(),
+        repository: new UsersRepository({
+            interceptors: [loadingInterceptor]
+        }),
     },
     mutations: {
         setUsers(state, payload) {
