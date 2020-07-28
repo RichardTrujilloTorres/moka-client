4a86b08454215f835ca7a7bfc6114e38b29aa573
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-07-28 05:00:15 +0200

    Add search components (cont.)

---
+++ src/components/Common/Search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="search">
+        <v-form @submit.prevent="onSearch">
+            <v-text-field
+                    v-model="query"
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Search"
+                    class="hidden-sm-and-down"
+            />
+        </v-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Search',
+        created() {
+        },
+        data () {
+            return {
+                query: ``,
+            }
+        },
+        methods: {
+            onSearch() {
+                return this.$route.name === 'Search' ?
+                    this.$router.replace({name: 'Search', query: {
+                            query: this.query,
+                    }})
+                    :
+                    this.$router.push({name: 'Search', query: {
+                            query: this.query,
+                    }});
+            },
+        }
+    }
+</script>
